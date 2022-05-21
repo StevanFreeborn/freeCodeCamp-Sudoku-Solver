25a78cb2080f48b34d5106db2657bd17a62db82d
StevanFreeborn <sfreeborn7@gmail.com>
2022-05-20 21:25:24 -0500

    implement SudokuSolver.validate() method and write unit tests for method

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,17 +1,26 @@
 class SudokuSolver {
 
     validate(puzzleString) {
+
+        return puzzleString.length == 81 && !puzzleString.match(/[^\.\d]/g);
+
     }
 
     checkRowPlacement(puzzleString, row, column, value) {
+
+
 
     }
 
     checkColPlacement(puzzleString, row, column, value) {
 
+
+
     }
 
     checkRegionPlacement(puzzleString, row, column, value) {
+
+        
 
     }
 
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,39 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+const { puzzlesAndSolutions } = require('../controllers/puzzle-strings');
+const Solver = require('../controllers/sudoku-solver');
+const solver = new Solver();
 
 suite('UnitTests', () => {
+
+    test('SudokuSolver.validate(puzzleString) valid puzzle string of 81 characters', (done) => {
+
+        const input = puzzlesAndSolutions[0][0];
+
+        assert.equal(solver.validate(input), true);
+        done();
+
+    });
+
+    test('SudokuSolver.validate(puzzleString) puzzle string of 81 characters with invalid characters', (done) => {
+
+        const input = puzzlesAndSolutions[0][0].replace(/\./g, '?');
+
+        assert.equal(solver.validate(input), false);
+        done();
+
+    });
+
+    test('SudokuSolver.validate(puzzleString) puzzle string of 82 characters with valid characters', (done) => {
+
+        const input = puzzlesAndSolutions[0][0] + '0';
+        
+        assert.equal(solver.validate(input), false);
+        done();
+
+    });
+
+
 
 });
